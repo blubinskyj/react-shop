6419f6067507ee3321f6a8e0aa05c0e8a5a4b0ef
blubinskyj <blubinskyj@gmail.com>
2022-06-03 17:22:30 +0300

    useContect instead state

--- src/components/GoodsList.jsx
+++ src/components/GoodsList.jsx
@@ -1,7 +1,9 @@
+import { useContext } from "react";
+import { ShopContext } from "../context";
 import { GoodsItem } from "./GoodsItem";
 
-function GoodsList(props) {
-  const { goods = [], addToBasket = Function.prototype } = props;
+function GoodsList() {
+  const { goods = [] } = useContext(ShopContext);
 
   if (!goods.length) {
     return <h3> Nothing </h3>;
@@ -10,7 +12,7 @@ function GoodsList(props) {
   return (
     <div className="goods">
       {goods.map((item) => (
-        <GoodsItem kay={item.id} {...item} addToBasket={addToBasket} />
+        <GoodsItem kay={item.id} {...item} />
       ))}
     </div>
   );
